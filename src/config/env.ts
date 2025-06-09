b4ac0e6525b831e47ea1aedc9462d21176547f04
Kimberly <kimberlymchelaa@gmail.com>
2025-06-09 18:52:12 -0400

    updated all page to show all and functioning

--- src/config/env.ts
+++
@@ -1,5 +0,0 @@
-export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || '';
-export const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || '';
-export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || '';
-export const PODCAST_INDEX_API_KEY = process.env.PODCAST_INDEX_API_KEY || '';
-export const PODCAST_INDEX_API_SECRET = process.env.PODCAST_INDEX_API_SECRET || ''; 
