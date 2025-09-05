9282947677fb78f3d8fe8fc20ad9ab4af9569048
nfldty <naufal.adityo16@gmail.com>
2025-09-05 02:10:47 -0400

    fix stripe version (?2)

--- src/app/api/payment/cancel-subscription/route.ts
+++ src/app/api/payment/cancel-subscription/route.ts
@@ -5,7 +5,7 @@ import { prisma } from '@/lib/prisma';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2025-06-30.basil',
+  apiVersion: '2025-08-27.basil',
 });
 
 export async function POST(request: NextRequest) {
