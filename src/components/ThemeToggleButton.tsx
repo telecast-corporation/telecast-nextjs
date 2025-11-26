630a48bbef182b9f48babc576a52e5c86d6ef715
dsam42751 <dsam42751@gmail.com>
2025-11-26 11:34:31 +0000

    fix: disable theme toggle button

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,19 +1,10 @@
 
 'use client';
 
-import { IconButton } from '@mui/material';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { useTheme } from '../contexts/ThemeContext';
+import React from 'react';
 
 const ThemeToggleButton = () => {
-  const { isDarkMode, toggleDarkMode } = useTheme();
-
-  return (
-    <IconButton sx={{ ml: 1 }} onClick={toggleDarkMode} color="inherit">
-      {isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
-    </IconButton>
-  );
+  return null;
 };
 
 export default ThemeToggleButton;
