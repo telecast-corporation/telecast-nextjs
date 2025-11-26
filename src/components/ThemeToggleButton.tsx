e74c2bb638ce94c263bc4152fa0c6a19eae0b2b4
dsam42751 <dsam42751@gmail.com>
2025-11-26 11:18:41 +0000

    fix: resolve error in ThemeToggleButton

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,19 +1,17 @@
 
 'use client';
 
-import { useContext } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-import { IconButton, useTheme } from '@mui/material';
+import { IconButton } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useTheme } from '../contexts/ThemeContext';
 
 const ThemeToggleButton = () => {
-  const theme = useTheme();
-  const { mode, toggleTheme } = useContext(ThemeContext);
+  const { isDarkMode, toggleDarkMode } = useTheme();
 
   return (
-    <IconButton sx={{ ml: 1 }} onClick={toggleTheme} color="inherit">
-      {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+    <IconButton sx={{ ml: 1 }} onClick={toggleDarkMode} color="inherit">
+      {isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
     </IconButton>
   );
 };
