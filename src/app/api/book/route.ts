255d04e8e8a1f2450cdc43c37826921d0bdc96b6
dsam42751 <dsam42751@gmail.com>
2025-11-30 21:40:01 +0000

    Implement fallback for book search

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,208 +1,171 @@
-import { NextResponse } from 'next/server';
-import axios from 'axios';
 
-const GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes';
+import { NextResponse } from 'next/server';
+import { google } from 'googleapis';
 
 const fallbackData = {
-  "kind": "books#volumes",
-  "totalItems": 40,
-  "items": [
-    {
-      "kind": "books#volume",
-      "id": "FgAjFqUSQq0C",
-      "etag": "kSjkXU0q424",
-      "selfLink": "https://www.googleapis.com/books/v1/volumes/FgAjFqUSQq0C",
-      "volumeInfo": {
-        "title": "Library of Congress Subject Headings",
-        "authors": [
-          "Library of Congress"
-        ],
-        "publishedDate": "2007",
-        "industryIdentifiers": [
-          {
-            "type": "OTHER",
-            "identifier": "OSU:32435076471762"
-          }
-        ],
-        "readingModes": {
-          "text": false,
-          "image": true
+    "kind": "books#volumes",
+    "totalItems": 1000000,
+    "items": [
+      {
+        "kind": "books#volume",
+        "id": "YwkSt9xAlbYC",
+        "etag": "1q29k4QmVjM",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/YwkSt9xAlbYC",
+        "volumeInfo": {
+          "title": "Numeric list of lenders",
+          "publishedDate": "19??",
+          "industryIdentifiers": [
+            {
+              "type": "OTHER",
+              "identifier": "COLUMBIA:CU14495155"
+            }
+          ],
+          "readingModes": {
+            "text": false,
+            "image": true
+          },
+          "pageCount": 812,
+          "printType": "BOOK",
+          "maturityRating": "NOT_MATURE",
+          "allowAnonLogging": false,
+          "contentVersion": "0.6.7.0.full.1",
+          "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+          },
+          "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=YwkSt9xAlbYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=YwkSt9xAlbYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          },
+          "language": "en",
+          "previewLink": "http://books.google.ca/books?id=YwkSt9xAlbYC&pg=PA392&dq=popular&hl=&as_pt=BOOKS&cd=1&source=gbs_api",
+          "infoLink": "https://play.google.com/store/books/details?id=YwkSt9xAlbYC&source=gbs_api",
+          "canonicalVolumeLink": "https://play.google.com/store/books/details?id=YwkSt9xAlbYC"
         },
-        "pageCount": 1512,
-        "printType": "BOOK",
-        "categories": [
-          "Subject headings, Library of Congress"
-        ],
-        "maturityRating": "NOT_MATURE",
-        "allowAnonLogging": false,
-        "contentVersion": "0.6.6.0.full.1",
-        "panelizationSummary": {
-          "containsEpubBubbles": false,
-          "containsImageBubbles": false
+        "saleInfo": {
+          "country": "CA",
+          "saleability": "FREE",
+          "isEbook": true,
+          "buyLink": "https://play.google.com/store/books/details?id=YwkSt9xAlbYC&rdid=book-YwkSt9xAlbYC&rdot=1&source=gbs_api"
         },
-        "imageLinks": {
-          "smallThumbnail": "http://books.google.com/books/content?id=FgAjFqUSQq0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-          "thumbnail": "http://books.google.com/books/content?id=FgAjFqUSQq0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        "accessInfo": {
+          "country": "CA",
+          "viewability": "ALL_PAGES",
+          "embeddable": true,
+          "publicDomain": true,
+          "textToSpeechPermission": "ALLOWED",
+          "epub": {
+            "isAvailable": false,
+            "downloadLink": "http://books.google.ca/books/download/Numeric_list_of_lenders.epub?id=YwkSt9xAlbYC&hl=&output=epub&source=gbs_api"
+          },
+          "pdf": {
+            "isAvailable": false
+          },
+          "webReaderLink": "http://play.google.com/books/reader?id=YwkSt9xAlbYC&hl=&as_pt=BOOKS&source=gbs_api",
+          "accessViewStatus": "FULL_PUBLIC_DOMAIN",
+          "quoteSharingAllowed": false
         },
-        "language": "en",
-        "previewLink": "http://books.google.com/books?id=FgAjFqUSQq0C&pg=PA5852&dq=popular&hl=&cd=1&source=gbs_api",
-        "infoLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C&source=gbs_api",
-        "canonicalVolumeLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C"
+        "searchInfo": {
+          "textSnippet": "... <b>POPULAR</b> DE PUERTO RICO 812423 BANCO <b>POPULAR</b> DE PUERTO RICO 812425 BANCO <b>POPULAR</b> DE PUERTO RICO 812426 BANCO <b>POPULAR</b> DE PUERTO RICO 812427 BANCO <b>POPULAR</b> DE PUERTO RICO 812428 BANCO <b>POPULAR</b> DE PUERTO RICO 812429 BANCO <b>POPULAR</b> DE PUERTO&nbsp;..."
+        }
       },
-      "saleInfo": {
-        "country": "US",
-        "saleability": "FREE",
-        "isEbook": true,
-        "buyLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C&rdid=book-FgAjFqUSQq0C&rdot=1&source=gbs_api"
-      },
-      "accessInfo": {
-        "country": "US",
-        "viewability": "ALL_PAGES",
-        "embeddable": true,
-        "publicDomain": true,
-        "textToSpeechPermission": "ALLOWED",
-        "epub": {
-          "isAvailable": false,
-          "downloadLink": "http://books.google.com/books/download/Library_of_Congress_Subject_Headings.epub?id=FgAjFqUSQq0C&hl=&output=epub&source=gbs_api"
+      {
+        "kind": "books#volume",
+        "id": "FgAjFqUSQq0C",
+        "etag": "UN9mOFa1yjs",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/FgAjFqUSQq0C",
+        "volumeInfo": {
+          "title": "Library of Congress Subject Headings",
+          "authors": [
+            "Library of Congress"
+          ],
+          "publishedDate": "2007",
+          "industryIdentifiers": [
+            {
+              "type": "OTHER",
+              "identifier": "OSU:32435076471762"
+            }
+          ],
+          "readingModes": {
+            "text": false,
+            "image": true
+          },
+          "pageCount": 1512,
+          "printType": "BOOK",
+          "categories": [
+            "Subject headings, Library of Congress"
+          ],
+          "maturityRating": "NOT_MATURE",
+          "allowAnonLogging": false,
+          "contentVersion": "0.6.6.0.full.1",
+          "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+          },
+          "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=FgAjFqUSQq0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=FgAjFqUSQq0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          },
+          "language": "en",
+          "previewLink": "http://books.google.ca/books?id=FgAjFqUSQq0C&pg=PA5852&dq=popular&hl=&as_pt=BOOKS&cd=2&source=gbs_api",
+          "infoLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C&source=gbs_api",
+          "canonicalVolumeLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C"
         },
-        "pdf": {
-          "isAvailable": false
+        "saleInfo": {
+          "country": "CA",
+          "saleability": "FREE",
+          "isEbook": true,
+          "buyLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C&rdid=book-FgAjFqUSQq0C&rdot=1&source=gbs_api"
         },
-        "webReaderLink": "http://play.google.com/books/reader?id=FgAjFqUSQq0C&hl=&source=gbs_api",
-        "accessViewStatus": "FULL_PUBLIC_DOMAIN",
-        "quoteSharingAllowed": false
-      },
-      "searchInfo": {
-        "textSnippet": "Library of Congress. \u003cb\u003ePopular\u003c/b\u003e culture ( Continued ) Motherhood in \u003cb\u003epopular\u003c/b\u003e culture Mountain people in \u003cb\u003epopular\u003c/b\u003e culture Muslims in \u003cb\u003epopular\u003c/b\u003e culture Older people in \u003cb\u003epopular\u003c/b\u003e culture Organization in \u003cb\u003epopular\u003c/b\u003e culture Police in \u003cb\u003epopular\u003c/b\u003e culture&nbsp;..."
+        "accessInfo": {
+          "country": "CA",
+          "viewability": "ALL_PAGES",
+          "embeddable": true,
+          "publicDomain": true,
+          "textToSpeechPermission": "ALLOWED",
+          "epub": {
+            "isAvailable": false,
+            "downloadLink": "http://books.google.ca/books/download/Library_of_Congress_Subject_Headings.epub?id=FgAjFqUSQq0C&hl=&output=epub&source=gbs_api"
+          },
+          "pdf": {
+            "isAvailable": false
+          },
+          "webReaderLink": "http://play.google.com/books/reader?id=FgAjFqUSQq0C&hl=&as_pt=BOOKS&source=gbs_api",
+          "accessViewStatus": "FULL_PUBLIC_DOMAIN",
+          "quoteSharingAllowed": false
+        },
+        "searchInfo": {
+          "textSnippet": "Library of Congress. <b>Popular</b> culture ( Continued ) Motherhood in <b>popular</b> culture Mountain people in <b>popular</b> culture Muslims in <b>popular</b> culture Older people in <b>popular</b> culture Organization in <b>popular</b> culture Police in <b>popular</b> culture&nbsp;..."
+        }
       }
-    }
-  ]
+    ]
 };
 
-export const dynamic = 'force-dynamic';
-
-// Helper function to convert HTTP URLs to HTTPS
-function ensureHttps(url: string | undefined): string | undefined {
-  if (!url) return url;
-  return url.replace(/^http:/, 'https');
-}
-
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get('q');
+
+  if (!query) {
+    return NextResponse.json({ error: 'Search query is required' }, { status: 400 });
+  }
+
   try {
-    const { searchParams } = new URL(request.url);
-    const query = searchParams.get('q');
-    const maxResults = searchParams.get('maxResults') || '20';
-    const startIndex = searchParams.get('startIndex') || '0';
-
-    // Enhanced environment logging
-    console.log('=== BOOKS SEARCH DEBUG INFO ===');
-    console.log('Environment:', {
-      nodeEnv: process.env.NODE_ENV,
-      vercelEnv: process.env.VERCEL_ENV,
-      hasApiKey: !!process.env.GOOGLE_BOOKS_API_KEY,
-      apiKeyLength: process.env.GOOGLE_BOOKS_API_KEY?.length,
-      apiKeyPrefix: process.env.GOOGLE_BOOKS_API_KEY?.substring(0, 10) + '...',
-      allEnvKeys: Object.keys(process.env).filter(key => key.includes('GOOGLE'))
-    });
-    console.log('Request URL:', request.url);
-    console.log('Search params:', { query, maxResults, startIndex });
-
-    if (!query) {
-      return NextResponse.json({ error: 'Query parameter is required' }, { status: 400 });
-    }
-
-    if (!process.env.GOOGLE_BOOKS_API_KEY) {
-      console.error('❌ Google Books API key is missing in production');
-      console.error('Available environment variables:', Object.keys(process.env).filter(key => key.includes('API')));
-      return NextResponse.json(
-        fallbackData,
-        { status: 500 }
-      );
-    }
-
-    // Log the actual request URL (without the API key)
-    const requestUrl = `${GOOGLE_BOOKS_API_URL}?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}&langRestrict=en&printType=books&orderBy=relevance`;
-    console.log('Making request to:', requestUrl);
-    console.log('API Key present:', !!process.env.GOOGLE_BOOKS_API_KEY);
-    const response = await axios.get(GOOGLE_BOOKS_API_URL, {
-      params: {
-        q: query,
-        maxResults,
-        startIndex,
-        langRestrict: 'en',
-        printType: 'books',
-        orderBy: 'relevance',
-        key: process.env.GOOGLE_BOOKS_API_KEY
-      },
-      timeout: 10000, // 10 second timeout
+    const books = google.books({
+      version: 'v1',
+      auth: process.env.GOOGLE_API_KEY,
     });
 
-    // Log response details
-    console.log('✅ Google Books API response:', {
-      status: response.status,
-      statusText: response.statusText,
-      itemsCount: response.data.items?.length || 0,
-      totalItems: response.data.totalItems,
-      hasError: !!response.data.error,
-      errorMessage: response.data.error?.message
+    const res = await books.volumes.list({
+      q: query,
+      maxResults: 20,
+      langRestrict: 'en',
+      printType: 'books',
+      orderBy: 'relevance',
     });
 
-    if (response.data.error) {
-      console.error('❌ Google Books API returned an error:', response.data.error);
-      return NextResponse.json(
-        fallbackData,
-        { status: 500 }
-      );
-    }
-
-    // Convert all image URLs to HTTPS
-    if (response.data.items) {
-      response.data.items = response.data.items.map((item: any) => {
-        if (item.volumeInfo?.imageLinks) {
-          Object.keys(item.volumeInfo.imageLinks).forEach(key => {
-            item.volumeInfo.imageLinks[key] = ensureHttps(item.volumeInfo.imageLinks[key]);
-          });
-        }
-        return item;
-      });
-    }
-
-    console.log('✅ Successfully processed and returning data');
-    return NextResponse.json(response.data);
-  } catch (error: any) {
-    // Enhanced error logging
-    console.error('❌ Books search error:', {
-      message: error.message,
-      code: error.code,
-      response: {
-        status: error.response?.status,
-        statusText: error.response?.statusText,
-        data: error.response?.data
-      },
-      config: {
-        url: error.config?.url,
-        method: error.config?.method,
-        timeout: error.config?.timeout
-      }
-    });
-
-    if (error.response?.status === 403) {
-      return NextResponse.json(
-        fallbackData,
-        { status: 429 }
-      );
-    }
-
-    if (error.code === 'ECONNABORTED') {
-      return NextResponse.json(
-        fallbackData,
-        { status: 408 }
-      );
-    }
-
-    return NextResponse.json(
-      fallbackData,
-      { status: 500 }
-    );
+    return NextResponse.json(res.data);
+  } catch (error) {
+    console.error('Google Books API error:', error);
+    return NextResponse.json(fallbackData, { status: 500, statusText: "API Failure" });
   }
 }
--- src/components/BookGrid.tsx
+++ src/components/BookGrid.tsx
@@ -18,6 +18,145 @@ const ensureHttps = (url: string | undefined): string => {
   return url.replace(/^http:/, 'https');
 };
 
+const fallbackData = {
+    "kind": "books#volumes",
+    "totalItems": 1000000,
+    "items": [
+      {
+        "kind": "books#volume",
+        "id": "YwkSt9xAlbYC",
+        "etag": "1q29k4QmVjM",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/YwkSt9xAlbYC",
+        "volumeInfo": {
+          "title": "Numeric list of lenders",
+          "publishedDate": "19??",
+          "industryIdentifiers": [
+            {
+              "type": "OTHER",
+              "identifier": "COLUMBIA:CU14495155"
+            }
+          ],
+          "readingModes": {
+            "text": false,
+            "image": true
+          },
+          "pageCount": 812,
+          "printType": "BOOK",
+          "maturityRating": "NOT_MATURE",
+          "allowAnonLogging": false,
+          "contentVersion": "0.6.7.0.full.1",
+          "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+          },
+          "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=YwkSt9xAlbYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=YwkSt9xAlbYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          },
+          "language": "en",
+          "previewLink": "http://books.google.ca/books?id=YwkSt9xAlbYC&pg=PA392&dq=popular&hl=&as_pt=BOOKS&cd=1&source=gbs_api",
+          "infoLink": "https://play.google.com/store/books/details?id=YwkSt9xAlbYC&source=gbs_api",
+          "canonicalVolumeLink": "https://play.google.com/store/books/details?id=YwkSt9xAlbYC"
+        },
+        "saleInfo": {
+          "country": "CA",
+          "saleability": "FREE",
+          "isEbook": true,
+          "buyLink": "https://play.google.com/store/books/details?id=YwkSt9xAlbYC&rdid=book-YwkSt9xAlbYC&rdot=1&source=gbs_api"
+        },
+        "accessInfo": {
+          "country": "CA",
+          "viewability": "ALL_PAGES",
+          "embeddable": true,
+          "publicDomain": true,
+          "textToSpeechPermission": "ALLOWED",
+          "epub": {
+            "isAvailable": false,
+            "downloadLink": "http://books.google.ca/books/download/Numeric_list_of_lenders.epub?id=YwkSt9xAlbYC&hl=&output=epub&source=gbs_api"
+          },
+          "pdf": {
+            "isAvailable": false
+          },
+          "webReaderLink": "http://play.google.com/books/reader?id=YwkSt9xAlbYC&hl=&as_pt=BOOKS&source=gbs_api",
+          "accessViewStatus": "FULL_PUBLIC_DOMAIN",
+          "quoteSharingAllowed": false
+        },
+        "searchInfo": {
+          "textSnippet": "... <b>POPULAR</b> DE PUERTO RICO 812423 BANCO <b>POPULAR</b> DE PUERTO RICO 812425 BANCO <b>POPULAR</b> DE PUERTO RICO 812426 BANCO <b>POPULAR</b> DE PUERTO RICO 812427 BANCO <b>POPULAR</b> DE PUERTO RICO 812428 BANCO <b>POPULAR</b> DE PUERTO RICO 812429 BANCO <b>POPULAR</b> DE PUERTO&nbsp;..."
+        }
+      },
+      {
+        "kind": "books#volume",
+        "id": "FgAjFqUSQq0C",
+        "etag": "UN9mOFa1yjs",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/FgAjFqUSQq0C",
+        "volumeInfo": {
+          "title": "Library of Congress Subject Headings",
+          "authors": [
+            "Library of Congress"
+          ],
+          "publishedDate": "2007",
+          "industryIdentifiers": [
+            {
+              "type": "OTHER",
+              "identifier": "OSU:32435076471762"
+            }
+          ],
+          "readingModes": {
+            "text": false,
+            "image": true
+          },
+          "pageCount": 1512,
+          "printType": "BOOK",
+          "categories": [
+            "Subject headings, Library of Congress"
+          ],
+          "maturityRating": "NOT_MATURE",
+          "allowAnonLogging": false,
+          "contentVersion": "0.6.6.0.full.1",
+          "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+          },
+          "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=FgAjFqUSQq0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=FgAjFqUSQq0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          },
+          "language": "en",
+          "previewLink": "http://books.google.ca/books?id=FgAjFqUSQq0C&pg=PA5852&dq=popular&hl=&as_pt=BOOKS&cd=2&source=gbs_api",
+          "infoLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C&source=gbs_api",
+          "canonicalVolumeLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C"
+        },
+        "saleInfo": {
+          "country": "CA",
+          "saleability": "FREE",
+          "isEbook": true,
+          "buyLink": "https://play.google.com/store/books/details?id=FgAjFqUSQq0C&rdid=book-FgAjFqUSQq0C&rdot=1&source=gbs_api"
+        },
+        "accessInfo": {
+          "country": "CA",
+          "viewability": "ALL_PAGES",
+          "embeddable": true,
+          "publicDomain": true,
+          "textToSpeechPermission": "ALLOWED",
+          "epub": {
+            "isAvailable": false,
+            "downloadLink": "http://books.google.ca/books/download/Library_of_Congress_Subject_Headings.epub?id=FgAjFqUSQq0C&hl=&output=epub&source=gbs_api"
+          },
+          "pdf": {
+            "isAvailable": false
+          },
+          "webReaderLink": "http://play.google.com/books/reader?id=FgAjFqUSQq0C&hl=&as_pt=BOOKS&source=gbs_api",
+          "accessViewStatus": "FULL_PUBLIC_DOMAIN",
+          "quoteSharingAllowed": false
+        },
+        "searchInfo": {
+          "textSnippet": "Library of Congress. <b>Popular</b> culture ( Continued ) Motherhood in <b>popular</b> culture Mountain people in <b>popular</b> culture Muslims in <b>popular</b> culture Older people in <b>popular</b> culture Organization in <b>popular</b> culture Police in <b>popular</b> culture&nbsp;..."
+        }
+      }
+    ]
+};
+
 interface Book {
   id: string;
   volumeInfo: {
@@ -58,7 +197,9 @@ export default function BookGrid({ searchQuery }: BookGridProps) {
         const data = await response.json();
         
         if (!response.ok && !data.items) {
-          throw new Error(data.error || 'Failed to fetch books');
+            setBooks(fallbackData.items);
+            setTotalItems(fallbackData.totalItems);
+            throw new Error(data.error || 'Failed to fetch books, using fallback data');
         }
         
         // Process books to ensure HTTPS URLs
