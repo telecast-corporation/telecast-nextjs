9e3afae484f73365925cb297e786775908666fab
dsam42751 <dsam42751@gmail.com>
2025-12-16 20:44:12 +0000

    fix: filter rejected news from main list

--- src/app/admin/local-news/page.tsx
+++ src/app/admin/local-news/page.tsx
@@ -19,7 +19,7 @@ import { db } from '../../../lib/dexie';
 
 const LocalNewsAdminPage = () => {
   const localNews = useLiveQuery(() =>
-    db.localNews.filter(news => news.isApproved !== true).toArray()
+    db.localNews.filter(news => news.isApproved !== true && news.isRejected !== true).toArray()
   );
 
   return (
